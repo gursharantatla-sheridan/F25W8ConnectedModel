69b117257e8aed840b60bdf10b82152e27423840
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-10-29 14:12:19 -0400

    load btn

--- F25W8ConnectedModel/MainWindow.xaml.cs
+++ F25W8ConnectedModel/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace F25W8ConnectedModel
 {
@@ -16,9 +19,35 @@ namespace F25W8ConnectedModel
     /// </summary>
     public partial class MainWindow : Window
     {
+        // read the connection string from the App.config file
+        string connStr = ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadGrid()
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+
+            string query = "select EmployeeID, FirstName, LastName, City, Country from Employees";
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            DataTable tbl = new DataTable();
+            tbl.Load(reader);
+            grdEmployees.ItemsSource = tbl.DefaultView;
+
+            conn.Close();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            LoadGrid();
         }
     }
 }
